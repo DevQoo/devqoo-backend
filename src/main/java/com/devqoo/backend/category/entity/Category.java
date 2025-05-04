e70a92ccce0171b867eb5c801bf915d46790e39d
badstar1004 <dmswjd4566@hanmail.net>
2025-05-04 16:28:48 +0900

    Feat: commonResponse 생성
    - commonResponse 생성
    - exception 수정
    - errorCode 수정

--- src/main/java/com/devqoo/backend/category/entity/Category.java
+++ src/main/java/com/devqoo/backend/category/entity/Category.java
@@ -16,17 +16,20 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "categories")
 @Getter
-@Builder
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id")
-    private Long id;
+    private Long categoryId;
 
     @Column(name = "category_name", nullable = false, length = 50)
     private String categoryName;
 
+
+    @Builder
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
