9a8c91d1c1deaaf3240f0e057013ca6f16ca8b99
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-05-05 18:06:45 +0900

    refactor: update CommonResponse class to improve readability and maintainability

--- src/main/java/com/devqoo/backend/common/response/CommonResponse.java
+++ src/main/java/com/devqoo/backend/common/response/CommonResponse.java
@@ -1,47 +1,39 @@
 package com.devqoo.backend.common.response;
 
 import com.devqoo.backend.common.exception.ErrorCode;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-<<<<<<< HEAD
-public final class CommonResponse<T> {
-
-    private final int statusCode;             // HTTP STATUS CODE
-    private final T data;                     // response data
-    private final String errorMessage;        // error message
-=======
 public class CommonResponse<T> {
 
-    private int statusCode;             // HTTP STATUS CODE
-    private T data;                     // response data
-    private String errorMessage;        // error message
->>>>>>> 029d993 (Feat: commonResponse 생성)
+	private int statusCode;             // HTTP STATUS CODE
+	private T data;                     // response data
+	private String errorMessage;        // error message
 
+	// ✅ success일 경우: data와 statusCode만 채움
+	public static <T> CommonResponse<T> success(
+		int statusCode,
+		T data
+	) {
+		return CommonResponse.<T>builder()
+			.statusCode(statusCode)
+			.data(data)
+			.errorMessage("")
+			.build();
+	}
 
-    // ✅ success일 경우: data와 statusCode만 채움
-    public static <T> CommonResponse<T> success(
-        int statusCode,
-        T data
-    ) {
-        return CommonResponse.<T>builder()
-            .statusCode(statusCode)
-            .data(data)
-            .errorMessage("")
-            .build();
-    }
-
-    // ✅ error일 경우: errorCode로부터 statusCode와 기본 메시지를 가져옴
-    public static <T> CommonResponse<T> error(
-        ErrorCode errorCode,
-        String errorMessage
-    ) {
-        return CommonResponse.<T>builder()
-            .statusCode(errorCode.getHttpStatus())
-            .data(null)
-            .errorMessage(errorMessage != null ? errorMessage : errorCode.getMessage())
-            .build();
-    }
+	// ✅ error일 경우: errorCode로부터 statusCode와 기본 메시지를 가져옴
+	public static <T> CommonResponse<T> error(
+		ErrorCode errorCode,
+		String errorMessage
+	) {
+		return CommonResponse.<T>builder()
+			.statusCode(errorCode.getHttpStatus())
+			.data(null)
+			.errorMessage(errorMessage != null ? errorMessage : errorCode.getMessage())
+			.build();
+	}
 }
