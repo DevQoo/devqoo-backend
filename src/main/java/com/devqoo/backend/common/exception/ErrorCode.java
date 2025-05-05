607c9e70e6621f1d118d7ab83b599fb80466baeb
badstar1004 <dmswjd4566@hanmail.net>
2025-05-05 18:37:17 +0900

    fix: 빠진 파일 추가

---
+++ src/main/java/com/devqoo/backend/common/exception/ErrorCode.java
@@ -0,0 +1,30 @@
+package com.devqoo.backend.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.")
+    ;
+
+
+    private final HttpStatus httpStatus;    // HTTP STATUS CODE
+    private final String message;           // error message
+
+
+    /* 생성자 */
+    ErrorCode(
+        HttpStatus httpStatus,
+        String message
+    ) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    /* Status int로 반환 */
+    public int getHttpStatus() {
+        return httpStatus.value();
+    }
+}
