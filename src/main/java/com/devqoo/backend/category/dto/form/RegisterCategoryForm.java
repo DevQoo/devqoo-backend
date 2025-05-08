6a7d44b881aeb24bfd1e70dd5abb6168a97eb123
백재우 <9712jw@gmail.com>
2025-05-08 22:15:36 +0900

    Feat: 카테고리 DTO Form 이름 제한 최소 2자 추가

--- src/main/java/com/devqoo/backend/category/dto/form/RegisterCategoryForm.java
+++ src/main/java/com/devqoo/backend/category/dto/form/RegisterCategoryForm.java
@@ -1,9 +1,11 @@
 package com.devqoo.backend.category.dto.form;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterCategoryForm(
     @NotBlank(message = "카테고리 이름은 필수입니다.")
+    @Min(value = 2, message = "카테고리 이름은 최소 2자 이상이어야 합니다.")
     String categoryName
 ) {
 
