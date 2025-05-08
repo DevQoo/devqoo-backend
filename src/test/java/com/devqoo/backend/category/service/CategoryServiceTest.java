aea73f8fe19318a1561fd275cad56ff360773102
백재우 <9712jw@gmail.com>
2025-05-08 22:25:10 +0900

    Feat: 카테고리 서비스 생성 테스트 추가
    카테고리 이름이 없을 경우에 카테고리 생성 테스트

---
+++ src/test/java/com/devqoo/backend/category/service/CategoryServiceTest.java
@@ -0,0 +1,53 @@
+package com.devqoo.backend.category.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.devqoo.backend.category.dto.form.RegisterCategoryForm;
+import com.devqoo.backend.category.entity.Category;
+import com.devqoo.backend.category.repository.CategoryRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+
+    public static final String CATEGORY_NAME = "질문 게시판";
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @InjectMocks
+    private CategoryService categoryService;
+
+    @DisplayName("create: 카테고리 생성 테스트")
+    @Test
+    void shouldCreateCategory_whenCategoryNameDoesNotExist() {
+        // given
+        RegisterCategoryForm form = new RegisterCategoryForm(CATEGORY_NAME);
+        Category category = new Category(CATEGORY_NAME);
+        given(categoryRepository.existsByCategoryName(CATEGORY_NAME)).willReturn(false);
+        given(categoryRepository.save(any(Category.class))).willReturn(category); // 이 줄 추가!
+
+        // when
+        Category createdCategory = categoryService.create(form);
+
+        // then
+        assertThat(createdCategory.getCategoryName()).isEqualTo(CATEGORY_NAME);
+    }
+
+    @DisplayName("create: 이미 존재하는 카테고리 이름이면 예외를 던진다")
+    @Test
+    void shouldThrowException_whenCategoryNameAlreadyExists() {
+        // given
+        RegisterCategoryForm form = new RegisterCategoryForm(CATEGORY_NAME);
+        given(categoryRepository.existsByCategoryName(CATEGORY_NAME)).willReturn(true);
+        // when
+
+        // then
+    }
+}
