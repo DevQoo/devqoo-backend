641b8edc527099fbe69b2f305cc4fb1fb67bb307
백재우 <9712jw@gmail.com>
2025-05-20 22:49:24 +0900

    Feat: 댓글 API 인터페이스, Request, Response 객체 생성

---
+++ src/main/java/com/devqoo/backend/comment/controller/CommentApiDocs.java
@@ -0,0 +1,15 @@
+package com.devqoo.backend.comment.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Comment", description = "댓글 관련 API 입니다.")
+public interface CommentApiDocs {
+
+    // 댓글 목록 조회
+    
+    // 댓글 생성
+
+    // 댓글 수정
+
+    // 댓글 삭제
+}
---
+++ src/main/java/com/devqoo/backend/comment/dto/form/RegisterCommentForm.java
@@ -0,0 +1,13 @@
+package com.devqoo.backend.comment.dto.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RegisterCommentForm(
+    @NotNull(message = "게시글 ID는 필수입니다.")
+    Long postId,
+    @NotBlank(message = "댓글 내용은 필수입니다.")
+    String content
+) {
+
+}
