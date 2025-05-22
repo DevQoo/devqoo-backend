1041305e0af0e069947867130a9cfba83eed3d76
badstar1004 <dmswjd4566@hanmail.net>
2025-05-22 23:09:35 +0900

    Fix: 게시글 수정 기능 변경
    - [x] 게시글 수정 기능 코드 변경
       - Facade 클래스 삭제
       - 게시글 생성, 수정 Form 겸용으로 변경

--- src/main/java/com/devqoo/backend/post/controller/PostController.java
+++ src/main/java/com/devqoo/backend/post/controller/PostController.java
@@ -1,19 +1,18 @@
 package com.devqoo.backend.post.controller;
 
 import com.devqoo.backend.common.response.CommonResponse;
-import com.devqoo.backend.post.dto.form.PostRegisterForm;
-import com.devqoo.backend.post.dto.form.PostUpdateForm;
+import com.devqoo.backend.post.dto.form.PostForm;
 import com.devqoo.backend.post.dto.response.PostResponseDto;
-import com.devqoo.backend.post.service.PostFacade;
+import com.devqoo.backend.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,18 +23,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PostController implements PostApiDocs {
 
-    private final PostFacade postFacade;
+    private final PostService postService;
 
 
     // 게시글 생성
     @Override
     @PostMapping
-    public ResponseEntity<CommonResponse<Long>> createPost(@RequestBody @Valid PostRegisterForm postRegisterForm) {
+    public ResponseEntity<CommonResponse<Long>> createPost(@RequestBody @Valid PostForm postForm) {
 
-        Long postId = postFacade.createPost(postRegisterForm);
+        Long postId = postService.createPost(postForm);
 
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(CommonResponse.success(HttpStatus.CREATED.value(), postId));
+    }
+
+    // 게시글 수정
+    @Override
+    @PutMapping("/{postId}")
+    public ResponseEntity<CommonResponse<Long>> updatePost(@PathVariable Long postId,
+        @RequestBody PostForm postForm) {
+
+        Long updatePostId = postService.updatePost(postId, postForm);
+
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(CommonResponse.success(HttpStatus.OK.value(), updatePostId));
     }
 
     // 게시글 상세 조회
@@ -43,21 +54,10 @@ public class PostController implements PostApiDocs {
     @GetMapping("/{postId}")
     public ResponseEntity<CommonResponse<PostResponseDto>> getPostDetail(@PathVariable Long postId) {
 
-        PostResponseDto postDto = postFacade.getPostDetail(postId);
+        PostResponseDto postDto = postService.getPostDetail(postId);
 
         return ResponseEntity.status(HttpStatus.OK)
             .body(CommonResponse.success(HttpStatus.OK.value(), postDto));
     }
 
-    // 게시글 수정
-    @Override
-    @PatchMapping("/{postId}")
-    public ResponseEntity<CommonResponse<Long>> updatePost(@PathVariable Long postId,
-        @RequestBody PostUpdateForm postUpdateForm) {
-
-        Long updatePostId = postFacade.updatePost(postId, postUpdateForm);
-
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body(CommonResponse.success(HttpStatus.CREATED.value(), updatePostId));
-    }
 }
--- src/main/java/com/devqoo/backend/post/repository/PostRepository.java
+++ src/main/java/com/devqoo/backend/post/repository/PostRepository.java
@@ -1,8 +1,22 @@
 package com.devqoo.backend.post.repository;
 
 import com.devqoo.backend.post.entity.Post;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    // 게시글 조회 (postId, userId, categoryId 기준)
+    @Query("""
+        SELECT p
+          FROM Post p
+         WHERE p.postId = :postId
+           AND p.category.categoryId = :categoryId
+           AND p.user.userId = :userId""")
+    Optional<Post> findPostForUpdate(
+        @Param("postId") Long postId,
+        @Param("categoryId") Long categoryId,
+        @Param("userId") Long userId);
 }
