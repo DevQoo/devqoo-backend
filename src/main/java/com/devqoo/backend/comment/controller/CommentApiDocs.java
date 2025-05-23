57fee1156be46354381a7d4434846d50a672df18
백재우 <9712jw@gmail.com>
2025-05-23 21:02:22 +0900

    Refactor : 주석 제거 - CommentApiDocs

--- src/main/java/com/devqoo/backend/comment/controller/CommentApiDocs.java
+++ src/main/java/com/devqoo/backend/comment/controller/CommentApiDocs.java
@@ -9,16 +9,12 @@ import org.springframework.http.ResponseEntity;
 @Tag(name = "Comment", description = "댓글 관련 API 입니다.")
 public interface CommentApiDocs {
 
-    // 댓글 목록 조회
     ResponseEntity<CommonResponse<CommentResponseDto>> getComments();
 
-    // 댓글 생성
     ResponseEntity<CommonResponse<Long>> createComment(RegisterCommentForm form);
 
-    // 댓글 수정
     ResponseEntity<CommonResponse<Long>> updateComment(Long commentId, RegisterCommentForm form);
 
-    // 댓글 삭제
     ResponseEntity<Void> deleteComment(Long commentId);
 
 }
