3e0df6c31f57497f50e9e7575084fcc4dd7c620b
백재우 <9712jw@gmail.com>
2025-05-23 21:04:45 +0900

    Feat : Comment Controller API 인터페이스 구현

--- src/main/java/com/devqoo/backend/comment/controller/CommentController.java
+++ src/main/java/com/devqoo/backend/comment/controller/CommentController.java
@@ -1,22 +1,39 @@
 package com.devqoo.backend.comment.controller;
 
+import com.devqoo.backend.comment.dto.form.RegisterCommentForm;
+import com.devqoo.backend.comment.dto.response.CommentResponseDto;
 import com.devqoo.backend.comment.service.CommentFacade;
+import com.devqoo.backend.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/comments")
-public class CommentController {
+public class CommentController implements CommentApiDocs {
 
     private final CommentFacade commentFacade;
 
-    // 댓글 목록 조회
+    @Override
+    public ResponseEntity<CommonResponse<CommentResponseDto>> getComments() {
+        return null;
+    }
 
-    // 댓글 생성
+    @Override
+    public ResponseEntity<CommonResponse<Long>> createComment(RegisterCommentForm form) {
+        return null;
+    }
 
-    // 댓글 수정
+    @Override
+    public ResponseEntity<CommonResponse<Long>> updateComment(Long commentId, RegisterCommentForm form) {
+        return null;
+    }
 
-    // 댓글 삭제
+    @Override
+    public ResponseEntity<Void> deleteComment(Long commentId) {
+        return null;
+    }
+
 }
