12148a9e40d08532e88ae7a61707eeb69b92bde2
백재우 <9712jw@gmail.com>
2025-05-27 21:44:33 +0900

    Refactor: Register Comment new Line

--- src/main/java/com/devqoo/backend/comment/dto/form/RegisterCommentForm.java
+++ src/main/java/com/devqoo/backend/comment/dto/form/RegisterCommentForm.java
@@ -7,8 +7,10 @@ public record RegisterCommentForm(
 
     @NotNull(message = "작성자 ID는 필수입니다.")
     Long authorId,  // TODO : SecurityContextHolder에서 가져오기
+
     @NotNull(message = "게시글 ID는 필수입니다.")
     Long postId,
+
     @NotBlank(message = "댓글 내용은 필수입니다.")
     String content
 ) {
